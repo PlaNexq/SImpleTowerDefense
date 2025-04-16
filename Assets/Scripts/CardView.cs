ed5b8ccb716043510138d72bee07e85db50dca3d
Ilya <shio2001@ya.ru>
2025-04-16 14:25:15 +0500

    stuff

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private SpriteRenderer image;
+    [SerializeField] private GameObject wrapper;
+}
---
+++ Assets/Scripts/HandView.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+
+public class HandView : MonoBehaviour
+{
+    [SerializeField] private SplineContainer splineContainer;
+    private readonly List<CardView> cards = new();
+    public IEnumerator AddCard(CardView card)
+    {
+        cards.Add(card);
+        yield return UpdateCardPosition(0.15f);
+    }
+    private IEnumerator UpdateCardPosition(float duration)
+    {
+        if (cards.Count == 0) yield break;
+        float cardSpacing = 1f / 10f;
+        float firstCardPosition = 0.5f - (cards.Count - 1) * cardSpacing / 2;
+        Spline spline = splineContainer.Spline;
+        for (int i = 0; i < cards.Count; i++)
+        {
+            float p = firstCardPosition + i * cardSpacing;
+            Vector3 splinePosition = spline.EvaluatePosition(p);
+            Vector3 forward = spline.EvaluateTangent(p);
+            Vector3 up = spline.EvaluateUpVector(p);
+            Quaternion rotation = Quaternion.LookRotation(-up, Vector3.Cross(-up, forward).normalized);
+            cards[i].transform.DOMove(splinePosition + transform.position + 0.01f * i * Vector3.back, duration);
+            cards[i].transform.DORotate(rotation.eulerAngles, duration);
+        }
+        yield return new WaitForSeconds(duration);
+    }
+}
